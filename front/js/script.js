c9b10f9f01bb0cb67dc5bc91e5388be665b67b28
taous <barkataous@yahoo.fr>
2022-11-21 13:46:37 +0100

    insertion des produits

--- front/js/script.js
+++ front/js/script.js
@@ -1,47 +1,4 @@
-/*//Récupération des données de l'API
- 
-//ajouter les produits dans la page d'acceuil
-
-
-//création du lien du produit
-console.log("#items", document.getElementById("items"));
-console.log(".items", document.getElementsByClassName("items"));
-
-
-
-/*function displayProducts(products) {
-  let conteneurProduits= document.getElementById("items");
-  //conteneurProduits.innerHTML="";*/
-/*const products = 8;
-for (let i = 0; i < products; i++) {
-         
-const produitLien = document.createElement("a");
-document.getElementById("items").appendChild(produitLien);
-produitLien.setAttribute("href", "./product.html?id=42");
-
-//création de la balise parent (article) et les balises enfants
-const produitArticle = document.createElement("article");
-document.getElementById("items").appendChild(produitArticle);
-
-//creation de la balise enfant (img) image du produit.
-const produitImg = document.createElement("img");
-produitImg.setAttribute("src", "imageUrl" );
-produitImg.setAttribute("alt", "Lorem ipsum dolor sit amet, Kanap name1");
-produitArticle.appendChild(produitImg);
-
-//creation de la balise (h3) le nom du produit.
-const produitNom = document.createElement("h3");
-produitNom.setAttribute("class", "productName");
-produitNom.innerHTML = "Kanap name1";
-produitArticle.appendChild(produitNom)
-
-//creation de la balise (p) description du produit.
-
-const produitDescription = document.createElement("p");
-produitDescription.setAttribute("class", "productDescription");
-produitDescription.innerHTML = "Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.";
-produitArticle.appendChild(produitDescription)
-}*/
+//Récupération des données de l'API
 fetch("http://localhost:3000/api/products")
   .then(function (res) {
     if (res.ok) {
@@ -52,33 +9,46 @@ fetch("http://localhost:3000/api/products")
     ajoutArticles(articles);
     console.log(articles);
   })
+  
   .catch(function (err) {
     console.log(err);
   });
 
-// Afficher tout les produits
+//  Insérer les produits dans la page d’accueil 
 
 function ajoutArticles(articles) {
-  let fragment = document.createDocumentFragment()
+  //let fragment = document.createDocumentFragment()
+  let produitconteneur=document.getElementById("items")
   for (let article of articles) {
-    let carte = document.createElement("a")
-    carte.setAttribute("href", "./product.html?id=${article._id}")
+
+    //création du lien (a) du produit
+    let lienElement = document.createElement("a")
+    lienElement.setAttribute("href", `./product.html?id=${article._id}`)
+
     //création de la balise parent (article) et les balises enfants
     let baliseArticle = document.createElement("article")
-    let image = document.createElement("img")
-    image.src = article.imageUrl
-    image.alt = article.altTxt
-    baliseArticle.appendChild(image)
-    let titre = document.createElement("h3")
-    titre.classList.add("productName")
-    titre.textContent = article.name
-    baliseArticle.appendChild(titre)
-    let parag = document.createElement("p")
-    parag.classList.add("productDescription")
-    parag.textContent = article.description
-    baliseArticle.appendChild(parag)
-    carte.appendChild(baliseArticle)
-    fragment.appendChild(carte)
+
+    //creation de la balise enfant (img) image du produit.
+    let imageElement = document.createElement("img")
+    imageElement.src = article.imageUrl;
+    imageElement.alt = article.altTxt;
+    baliseArticle.appendChild(imageElement)
+
+    //creation de la balise (h3) le nom du produit.
+    let titreElement = document.createElement("h3")
+    titreElement.classList.add("productName")
+    titreElement.textContent = article.name
+    baliseArticle.appendChild(titreElement)
+
+    //creation de la balise (p) description du produit.
+    let paragElement = document.createElement("p")
+    paragElement.classList.add("productDescription")
+    paragElement.textContent = article.description
+    baliseArticle.appendChild(paragElement)
+
+    lienElement.appendChild(baliseArticle)
+    //fragment.appendChild(carte)
+    produitconteneur.appendChild(lienElement)
   }
-  document.getElementById("items").appendChild(fragment)
+ // document.getElementById("items").appendChild(fragment)
 }
